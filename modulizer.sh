ae3e30013c67f867005736ee742ac1e2ab7bf9cf
Paul Varache <paul@kano.me>
2018-08-15 14:44:59 +0100

    Bump version

--- modulizer.sh
+++ modulizer.sh
@@ -1,6 +1,6 @@
 modulizer --out . \
 --npm-name @kano/kwc-follow-item \
---npm-version 3.0.0-beta.6 \
+--npm-version 3.0.0-beta.7 \
 --import-style name \
 --dependency-mapping kwc-style,@kano/kwc-style,^3.0.0-beta.2
 
